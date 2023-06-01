ddb025e51c12e8200c6f46d3ac5de86c66a82e29
github Junichi-Machii <junichi878338@hotmail.co.jp>
2023-06-01 20:13:49 +0900

    test

--- .history/astro.config_20230601201150.mjs
+++ .history/astro.config_20230601201150.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://stargazers.club',
+  
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
   }), tailwind(), sitemap()]
